e0673ee48807be87d6f0dd554e66f8c5c3d6bbd8
Bloctans <93960460+Bloctans@users.noreply.github.com>
2022-03-15 17:23:51 -0500

    upgrade to v3 and tnix debug

--- main.js
+++ main.js
@@ -1,104 +1,52 @@
 var debugmode = true
 
-window.EventEmitter = class EventEmitter {
-        constructor() {
-            this.events = {};
-        }
-        on(event, cb) {
-            if (typeof this.events[event] !== 'object')
-                this.events[event] = [];
-            this.events[event].push(cb);
-        }
-        emit(event, data) {
-            if (typeof this.events[event] !== 'object')
-                return;
-            this.events[event].forEach((cb) => cb(data));
-        }
+class Cloudlink {
+    constructor(server) {
+        this.events = {};
+        this.ws = new WebSocket(server);
+        this.ws.onopen = async () => {
+            this.send({
+                cmd: 'direct',
+                val: {
+                    cmd: 'ip',
+                    val: await (await fetch('https://api.meower.org/ip')).text(),
+                },
+            });
+            this.send({
+                cmd: 'direct',
+                val: { cmd: 'type', val: 'js' },
+            });
+            this.emit('connected');
+        };
+        this.ws.onmessage = (socketdata) => {
+            var data = JSON.parse(socketdata.data);
+            console.log(data)
+            this.emit(data.cmd, data);
+        };
+        this.ws.onclose = () => {
+            this.emit('disconnected');
+        };
+        this.ws.onerror = (e) => {
+            this.emit('error', e);
+        };
     }
-    class Cloudlink {
-        constructor(server) {
-            this.events = new EventEmitter();
-            this.ws = new WebSocket(server);
-            this.data = {
-                gvar: {},
-                pvar: {},
-                ulist: [],
-                version: '0.0.0',
-                motd: '',
-                status: ''
-            };
-            this.ws.onopen = async () => {
-                this.send({
-                    cmd: 'direct',
-                    val: {
-                        cmd: 'ip',
-                        val: await (await fetch('https://api.meower.org/ip')).text(),
-                    },
-                });
-                this.send({
-                    cmd: 'direct',
-                    val: { cmd: 'type', val: 'js' },
-                });
-                this.send({
-                    cmd: 'direct',
-                    val: "meower",
-                });
-                this.events.emit('connected');
-            };
-            this.ws.onmessage = (socketdata) => {
-                var data = JSON.parse(socketdata.data);
-                if (debugmode == true) {
-                	console.log(data);
-                }
-                switch (data.cmd) {
-                    case 'gmsg':
-                        this.events.emit('gmsg', data);
-                    case 'pmsg':
-                        this.events.emit('pmsg', data);
-                    case 'gvar':
-                        this.data.gvar[data.name] = data.val;
-                        this.events.emit('gvar', data);
-                    case 'pvar':
-                        this.data.pvar[data.name] = data.val;
-                        this.events.emit('pvar', data);
-                    case 'ulist':
-                        this.data.ulist = data.val.split(';');
-                        this.events.emit('ulist', this.data.ulist);
-                    case 'direct':
-                        switch (data.val.cmd) {
-                            case 'vers':
-                                this.data.version = data.val.val;
-                                this.events.emit('version', this.data.version);
-                            case 'motd':
-                                this.data.motd = data.val.val;
-                                this.events.emit('motd', this.data.motd);
-                            default:
-                                this.events.emit('direct', data);
-                        }
-                    case 'statuscode':
-                        this.data.status = data.val;
-                        this.events.emit('statuscode', this.data.status);
-                    default:
-                        this.events.emit('error', new Error('Unknown command: ' + data.cmd));
-                }
-            };
-            this.ws.onclose = () => {
-                this.events.emit('disconnected');
-            };
-            this.ws.onerror = (e) => {
-                this.events.emit('error', e);
-            };
-        }
-        send(data) {
-            this.ws.send(JSON.stringify(data));
-        }
-        on(event, cb) {
-            this.events.on(event, cb);
-        }
-        disconnect() {
-            this.ws.close();
-        }
+    send(data) {
+        this.ws.send(JSON.stringify(data));
     }
+    on(event, cb) {
+        if (typeof this.events[event] !== 'object')
+            this.events[event] = [];
+        this.events[event].push(cb);
+    }
+    emit(event, data) {
+        if (typeof this.events[event] !== 'object')
+            return;
+        this.events[event].forEach((cb) => cb(data));
+    }
+    disconnect() {
+        this.ws.close();
+    }
+}
 
 
 
@@ -151,6 +99,25 @@ function clearposts() {
 
 function auth_login() {
     cljs.send({ cmd: "direct", val: {cmd: "authpswd", val: {username: document.getElementById("user1").value, pswd: document.getElementById("pswd1").value}}, listener: "authpswd"})
+    cljs.on('statuscode', (data) => {
+        console.log(data.val)
+        if (data.listener == "authpswd") {
+            console.log("auth")
+        }
+        if (data == "E:103 | ID not found") {
+            console.log("h")
+            ms_alert('Login Screen','Invalid Username!')
+        }
+        else if (data == "I:011 | Invalid Password") {
+            ms_alert('Login Screen','Invalid Password!')
+        }
+        //else if (data == "E:018 | Account Banned") {
+            //ms_alert('Login Screen','The account You have Attempted to Log Into is Banned!')
+        else {
+
+            ms_alert('Login Screen','Unexpected ' + data + ' Error!')
+        }
+    })
 }
 
 async function gotologin() {
@@ -219,16 +186,6 @@ async function goto_connect() {
         else if (data.cmd == "ulist") {
             ulist = data.val
             updateusercount()
-        }
-    })
-
-    cljs.on('statuscode', (data) => {
-        if (data == "E:103 | ID not found") {
-            console.log("h")
-            ms_alert('Login Screen','Invalid Username!')
-        }
-        else if (data == "I:011 | Invalid Password") {
-            ms_alert('Login Screen','Invalid Password!')
         }
     })
 }
